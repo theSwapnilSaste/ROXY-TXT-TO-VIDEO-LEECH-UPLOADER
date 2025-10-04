1f347b9506da3fd0f947a2d6535544d9f2284cfa
𝕽𝕺𝕏𝕐•𝔹𝕒𝕤𝕚𝕔ℕ𝕖𝕖𝕕𝔹𝕠𝕥 ⚡️ <roxybasicneedbot@zohomail.in>
2025-10-04 13:06:57 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,21 +2,22 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "write yours"))
-API_HASH = environ.get("API_HASH", "write yours")
-BOT_TOKEN = environ.get("BOT_TOKEN", "write yours")
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Force Subscribe Configuration
 FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "roxybasicneedbot1")  # Channel username without @, 
 FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "https://t.me/roxybasicneedbot1")  # Channel link
 
 # Admin Configuration
-ADMINS = list(map(int, environ.get("ADMINS", "write yours").split()))
+ADMINS = list(map(int, environ.get("ADMINS", "").split()))
 
 # Optional: Bot Owner ID
-OWNER_ID = int(environ.get("OWNER_ID", "write yours"))
+OWNER_ID = int(environ.get("OWNER_ID", ""))
 
 # Database URL (if you want to add database support later)
 DATABASE_URL = environ.get("DATABASE_URL", "write yours")
+
 
 
